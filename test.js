6ddc8503f6270a9fe57076de453361dec4f53f1a
RealSong88 <july2day@gmail.com>
2022-01-17 17:06:48 +0900

    eslint, prettier setting..

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ function init() {
   const single_q = 'hello world';
   const double_q = 'hello world';
   const no_semi = 500;
-  console.log('hello world!');
+  console.log('hello world!', single_q);
 }
 init();
