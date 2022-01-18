79faf696390eef72c3fc84cf3045d065fcee33e4
RealSong88 <july2day@gmail.com>
2022-01-19 06:34:20 +0900

    3.4 complete

--- test.js
+++ test.js
@@ -4,4 +4,11 @@ function init() {
   const no_semi = 500;
   console.log('hello world!');
 }
+
 init();
+function init() {
+  const single_q = 'hello world';
+  const double_q = 'hello world';
+  const no_semi = 500;
+  console.log('hello world!');
+}
